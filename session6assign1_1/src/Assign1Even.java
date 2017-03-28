ae0e977252829a5b5a7216a43f559838cd4b24f2
VrushaliB25 <vrbhoite@gmail.com>
2017-03-28 15:02:38 +0530

    Session6 Assignment1.1

---
+++ session6assign1_1/src/Assign1Even.java
@@ -0,0 +1,17 @@
+
+public class Assign1Even {
+	public static void main(String s[])
+	{
+		int a[]={2,4,6,8,10,12,14,16,18,20};
+		
+		System.out.println("The even numbers in arrary are as follows:");
+		for (int i=0; i<10;i++)
+		{
+		System.out.println(" "+a[i]);	
+		}
+		
+		
+		
+	}
+
+}
